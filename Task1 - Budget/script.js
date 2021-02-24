6d172e44cc0c88771c07cd8b2b67047afd9de7ee
Yaroslav Novikov <jarosnov@gmail.com>
2021-02-24 06:24:50 -0800

    Budget program. Something chahges.

--- Task1 - Budget/script.js
+++ Task1 - Budget/script.js
@@ -1,47 +1,24 @@
-/*
-1) Переписать наш цикл
-for еще двумя способами и закомментировать их(еще 2 вида циклов, тренируемся)
-
-2) Проверить, чтобы все работало и не было ошибок в консоли
-
-3) Добавить папку со вторым уроком в свой репозиторий на GitHub
-*/
-// var money,
-//     time;
-
 let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-// money = prompt("Ваш бюджет на месяц?");
-// time = prompt("Введите дату в формате YYYY-MM-DD");
-
 let appData = {
     budget: money,
-    expenses: {},
+    expenses: {}, // обязательные расходы
     income: [], // массив данных с доп. доходом
     timeData: time,
     savings: false
 };
 
-/*
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется", '');
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-*/
-
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется", '');
+
     if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
         a != '' && b != '' && a.length < 50) {
         console.log('done');
         appData.expenses[a] = b;
     } else {
-        appData.expenses[a] = b;
+        i = i - 1;
     }
 };
 
@@ -55,9 +32,9 @@ for (let i = 0; i < 2; i++) {
 //         console.log('done');
 //         appData.expenses[a] = b;
 //     } else {
-//         appData.expenses[a] = b;
+//         i = i - 1;
 //     }
-// };
+// }
 
 // let i = 0;
 // do {
@@ -69,7 +46,7 @@ for (let i = 0; i < 2; i++) {
 //         console.log('done');
 //         appData.expenses[a] = b;
 //     } else {
-//         appData.expenses[a] = b;
+//         i = i - 1;
 //     }
 // };
 // while (i < 2)
