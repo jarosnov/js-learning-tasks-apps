7c9fd48c069b47c2a2c034f32ce59425309508ee
Yaroslav Novikov <jarosnov@gmail.com>
2021-02-24 10:48:38 -0800

    Task3. Get elements from DOM.

--- Task3 - Budget DOM manipulate/js/main.js
+++ Task3 - Budget DOM manipulate/js/main.js
@@ -18,3 +18,30 @@
 
 3) Добавить папку с уроком на свой GitHub
 */
+
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'), // input - Введите обязательные расходы
+
+    expensesItemBtn = document.getElementsByTagName('button')[0], // button - Утвердить. Введите обязательные расходы
+    optExpensesBtn = document.getElementsByTagName('button')[1], // button - Утвердить. Введите необязательные расходы 
+    countBudgetBtn = document.getElementsByTagName('button')[2], // button - Рассчитать. Расчет дневного бюджета 
+
+    optExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
